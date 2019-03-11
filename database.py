81cb170c241fae9a4f0a5c1118b18482e7e11345
loucadufault <40028187+loucadufault@users.noreply.github.com>
2019-03-11 09:57:05 -0400

    Update database.py
    Added DELIM constant, removed file appending section from within the for loop of the file reading section in write_record, removed last name from line 21 since in other parts of the code there is only name age and password

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+DELIM = ', '
+
 def read_record(first):
     with open('db.txt', 'r') as f:
         for line in f:
             line = line.strip()
             if line.startswith(first):
-                frags = line.split(',')
+                frags = line.split(DELIM)
                 try:
                     frags[1] = int(frags[1])
                 except ValueError:
@@ -15,12 +17,12 @@ def write_record(name, age, password):
     with open('db.txt', 'r') as f:
         for line in f:
             line = line.strip()
-            frags = line.split(',')
-            if (frags == [first, last, age, password]):
+            frags = line.split(DELIM)
+            if (frags == [first, age, password]):
                 print('record already exists')
                 return
-            with open('db.txt', 'a+') as f:
-                f.write("{}, {}, {}".format(name, age, password))
+    with open('db.txt', 'a+') as f:
+        f.write("{}, {}, {}".format(name, age, password))
 
 
 write_record('john', 20, 'hello123')
